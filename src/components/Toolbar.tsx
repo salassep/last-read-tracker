fab06368e290cca59c3a4460145ce80599683cbe
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-11-22 21:47:46 +0800

    feat: create popover ui

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,3 +1,4 @@
+import PopOver from './popover/popover';
 import './toolbar.css';
 
 export default function Toolbar({ 
@@ -97,11 +98,7 @@ export default function Toolbar({
             <path fill="#ffffffde" d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11zm-6 4q-.825 0-1.412-.587T4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413T18 20z" />
           </svg>
         </button>
-        <button>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-            <path fill="#ffffffde" d="M12 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2" />
-          </svg>
-        </button>
+        <PopOver />
       </div>
     </div>
   );
---
+++ src/components/popover/PopOver.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from "react";
+import './pop-over.css';
+
+export default function PopOver(): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const togglePopover = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (popoverRef.current && event.target instanceof Node && !popoverRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="popover" ref={popoverRef}>
+      <button onClick={togglePopover} style={{ cursor: 'pointer' }}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+          <path fill="#ffffffde" d="M12 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2" />
+        </svg>
+      </button>
+      {isOpen && (
+        <ul className="popover-options">
+          <li>
+            Disable autosave
+          </li>
+          <li>
+            Disable autosave for this file
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
