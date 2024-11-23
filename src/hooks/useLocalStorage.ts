4b1bc3766a19fb4a42c8f126d14d0dfd07ec1dd2
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-11-23 15:13:54 +0800

    feat: check autosave before set and get from local storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,16 +1,52 @@
 import { useState, useEffect } from 'react';
+import { State } from '../types/state';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isState(obj: any): obj is State {
+  return (
+    obj &&
+    typeof obj === "object" &&
+    "activePage" in obj &&
+    typeof obj.activePage === "number" &&
+    "rotate" in obj &&
+    typeof obj.rotate === "number" &&
+    "scale" in obj &&
+    typeof obj.scale === "number"
+  );
+}
+
+function isAutosaveDisable(fileName: string): boolean {
+  const disableAutosave = localStorage.getItem('disableAutosave');
+  if (disableAutosave && JSON.parse(disableAutosave)) {
+    return true;
+  }
+
+  const exceptionFiles = localStorage.getItem('autosaveExceptionFiles');
+  if (exceptionFiles) {
+    const parsedExceptionFiles: string[] = JSON.parse(exceptionFiles);
+    if (parsedExceptionFiles.includes(fileName)) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 export default function useLocalStorage<T>(key: string, defaultValue: T) {
   const [value, setValue] = useState<T>(() => {
+    if (isState(defaultValue) && isAutosaveDisable(key)) return defaultValue;
+
     const storedValue = localStorage.getItem(key);
     if (storedValue) {
       return JSON.parse(storedValue) as T;
     }
+    
     return defaultValue;
   });
 
   useEffect(() => {
     if (value === undefined) return;
+    if (isState(value) && isAutosaveDisable(key)) return;
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
